7cd0056d39ad4c4d6f73212d9ae908b130134636
DESKTOP-26PKT0I\yamak <yamaken0123456789@gmail.com>
2021-02-05 17:46:18 +0900

    finished making the music

--- music/music/lib/main.dart
+++ music/music/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'music app'), /* ここで上に表示する文字を変えられる */
     );
   }
 }
@@ -28,11 +30,62 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //シンバル用
+  final cymbalPlayer = AudioCache(fixedPlayer: AudioPlayer());
+  AudioPlayer cymbalAudioSource;
+  // ギター用Audio
+  final guitarPlayer = AudioCache(fixedPlayer: AudioPlayer());
+  AudioPlayer guitarAudioSource;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+      ),
+      body: Stack(
+        children: [
+          Image.asset(
+            "images/background.png",
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.fitHeight,
+          ),
+          SafeArea(
+            child: Center(
+              child: Row(
+                /* 横に並べる */
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  FlatButton(
+                      onPressed: () async {
+                        /* 非同期関数として定義される */
+                        if (cymbalAudioSource != null) {
+                          cymbalAudioSource.stop();
+                        }
+                        /* シンバルを再生 */
+                        cymbalAudioSource = await cymbalPlayer.play(
+                            "cymbalSound.mp3",
+                            mode: PlayerMode.LOW_LATENCY);
+                      },
+                      child: Image.asset('images/cymbal.png')),
+                  FlatButton(
+                      onPressed: () async {
+                        if (guitarAudioSource != null) {
+                          // すでに再生中なら一旦停止する
+                          guitarAudioSource.stop();
+                        }
+                        // AudioCacheを用いて再生
+                        guitarAudioSource = await guitarPlayer.play(
+                            'guitarSound.mp3',
+                            mode: PlayerMode.LOW_LATENCY);
+                      },
+                      child: Image.asset('images/guitar.png')),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
