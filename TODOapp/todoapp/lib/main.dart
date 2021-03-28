06ff6bef266ce53e0842cd62a1cc6fa46bc6f0d6
DESKTOP-26PKT0I\yamak <yamaken0123456789@gmail.com>
2021-03-28 22:51:24 +0900

    login + TODO

--- TODOapp/todoapp/lib/main.dart
+++ TODOapp/todoapp/lib/main.dart
@@ -11,7 +11,6 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,35 +23,11 @@ class MyApp extends StatelessWidget {
   }
 }
 
+/* ログイン画面 */
 class MyAuthPage extends StatefulWidget {
   @override
   _MyAuthPageState createState() => _MyAuthPageState();
 }
-
-// class MyTodoApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       // 右上に表示される"debug"ラベルを消す
-//       debugShowCheckedModeBanner: false,
-//       // アプリ名
-//       title: 'My Todo App',
-//       theme: ThemeData(
-//         // テーマカラー
-//         primarySwatch: Colors.blue,
-//         visualDensity: VisualDensity.adaptivePlatformDensity,
-//       ),
-//       // リスト一覧画面を表示
-//       home: TodoListPage(),
-//     );
-//   }
-// }
-
-// // リスト一覧画面用Widget
-// class TodoListPage extends StatefulWidget {
-//   @override
-//   _TodoListPageState createState() => _TodoListPageState();
-// }
 
 class _MyAuthPageState extends State<MyAuthPage> {
   // 入力されたメールアドレス
@@ -155,6 +130,11 @@ class _MyAuthPageState extends State<MyAuthPage> {
                     final User user = result.user!;
                     setState(() {
                       infoText = "ログインOK：${user.email}";
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => TodoListPage(),
+                          ));
                     });
                   } catch (e) {
                     // ログインに失敗した場合
@@ -175,119 +155,125 @@ class _MyAuthPageState extends State<MyAuthPage> {
   }
 }
 
-// class _TodoListPageState extends State<TodoListPage> {
-//   // Todoリストのデータ
-//   List<String> todoList = [];
+// リスト一覧画面用Widget
+class TodoListPage extends StatefulWidget {
+  @override
+  _TodoListPageState createState() => _TodoListPageState();
+}
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       // AppBarを表示し、タイトルも設定
-//       appBar: AppBar(
-//         title: Text('リスト一覧'),
-//       ),
-//       // データを元にListViewを作成
-//       body: ListView.builder(
-//         itemCount: todoList.length,
-//         itemBuilder: (context, index) {
-//           return Card(
-//             child: ListTile(
-//               title: Text(todoList[index]),
-//             ),
-//           );
-//         },
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () async {
-//           // "push"で新規画面に遷移
-//           // リスト追加画面から渡される値を受け取る
-//           final newListText = await Navigator.of(context).push(
-//             MaterialPageRoute(builder: (context) {
-//               // 遷移先の画面としてリスト追加画面を指定
-//               return TodoAddPage();
-//             }),
-//           );
-//           if (newListText != null) {
-//             // キャンセルした場合は newListText が null となるので注意
-//             setState(() {
-//               // リスト追加
-//               todoList.add(newListText);
-//             });
-//           }
-//         },
-//         child: Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
+class _TodoListPageState extends State<TodoListPage> {
+  // Todoリストのデータ
+  List<String> todoList = [];
 
-// class TodoAddPage extends StatefulWidget {
-//   @override
-//   _TodoAddPageState createState() => _TodoAddPageState();
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // AppBarを表示し、タイトルも設定
+      appBar: AppBar(
+        title: Text('リスト一覧'),
+      ),
+      // データを元にListViewを作成
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(todoList[index]),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          // "push"で新規画面に遷移
+          // リスト追加画面から渡される値を受け取る
+          final newListText = await Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              // 遷移先の画面としてリスト追加画面を指定
+              return TodoAddPage();
+            }),
+          );
+          if (newListText != null) {
+            // キャンセルした場合は newListText が null となるので注意
+            setState(() {
+              // リスト追加
+              todoList.add(newListText);
+            });
+          }
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
 
-// class _TodoAddPageState extends State<TodoAddPage> {
-//   // 入力されたテキストをデータとして持つ
-//   String _text = '';
+class TodoAddPage extends StatefulWidget {
+  @override
+  _TodoAddPageState createState() => _TodoAddPageState();
+}
 
-//   // データを元に表示するWidget
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('リスト追加'),
-//       ),
-//       body: Container(
-//         // 余白を付ける
-//         padding: EdgeInsets.all(64),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             // 入力されたテキストを表示
-//             Text(_text, style: TextStyle(color: Colors.blue)),
-//             const SizedBox(height: 8),
-//             // テキスト入力
-//             TextField(
-//               // 入力されたテキストの値を受け取る（valueが入力されたテキスト）
-//               onChanged: (String value) {
-//                 // データが変更したことを知らせる（画面を更新する）
-//                 setState(() {
-//                   // データを変更
-//                   _text = value;
-//                 });
-//               },
-//             ),
-//             const SizedBox(height: 8),
-//             Container(
-//               // 横幅いっぱいに広げる
-//               width: double.infinity,
-//               // リスト追加ボタン
-//               child: ElevatedButton(
-//                 onPressed: () {
-//                   // "pop"で前の画面に戻る
-//                   // "pop"の引数から前の画面にデータを渡す
-//                   Navigator.of(context).pop(_text);
-//                 },
-//                 child: Text('リスト追加', style: TextStyle(color: Colors.white)),
-//               ),
-//             ),
-//             const SizedBox(height: 8),
-//             Container(
-//               // 横幅いっぱいに広げる
-//               width: double.infinity,
-//               // キャンセルボタン
-//               child: TextButton(
-//                 // ボタンをクリックした時の処理
-//                 onPressed: () {
-//                   // "pop"で前の画面に戻る
-//                   Navigator.of(context).pop();
-//                 },
-//                 child: Text('キャンセル'),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+class _TodoAddPageState extends State<TodoAddPage> {
+  // 入力されたテキストをデータとして持つ
+  String _text = '';
+
+  // データを元に表示するWidget
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('リスト追加'),
+      ),
+      body: Container(
+        // 余白を付ける
+        padding: EdgeInsets.all(64),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // 入力されたテキストを表示
+            Text(_text, style: TextStyle(color: Colors.blue)),
+            const SizedBox(height: 8),
+            // テキスト入力
+            TextField(
+              // 入力されたテキストの値を受け取る（valueが入力されたテキスト）
+              onChanged: (String value) {
+                // データが変更したことを知らせる（画面を更新する）
+                setState(() {
+                  // データを変更
+                  _text = value;
+                });
+              },
+            ),
+            const SizedBox(height: 8),
+            Container(
+              // 横幅いっぱいに広げる
+              width: double.infinity,
+              // リスト追加ボタン
+              child: ElevatedButton(
+                onPressed: () {
+                  // "pop"で前の画面に戻る
+                  // "pop"の引数から前の画面にデータを渡す
+                  Navigator.of(context).pop(_text);
+                },
+                child: Text('リスト追加', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Container(
+              // 横幅いっぱいに広げる
+              width: double.infinity,
+              // キャンセルボタン
+              child: TextButton(
+                // ボタンをクリックした時の処理
+                onPressed: () {
+                  // "pop"で前の画面に戻る
+                  Navigator.of(context).pop();
+                },
+                child: Text('キャンセル'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
